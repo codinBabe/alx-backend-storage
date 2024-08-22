8750dd7d654395a600958a5f10a5ff9b432fb89e
codinBabe <teboola2@gmail.com>
2024-08-22 18:55:40 +0000

    fix:check fail in task 10-11

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,14 @@
 -- A script that creates a function that safely divides two numbers.
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURN FLOAT DETERMINISTIC
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-DECLARE ret FLOAT DEFAULT 0;
-    IF b <> 0 THEN
-    SET ret = a / b;
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
     END IF;
-    RETURN ret;
+    RETURN result;
 END $$
 DELIMITER ;
--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,12 @@
--- A script that creates a view named need_meeting that lists the names of students who have a score less than 80 and have not had a meeting in the last month.
+-- A script that creates a view named need_meeting that lists the names of students
+-- who have a score less than 80 and have not had a meeting in the last month.
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT names
-FROM students
-WHERE scores < 80
-(last_meeting IS NULL
-OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
