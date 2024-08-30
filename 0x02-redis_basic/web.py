761d0307e8d42c68c9af113e7805b5311d63cbe5
codinBabe <teboola2@gmail.com>
2024-08-30 03:00:29 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,38 +5,29 @@
 import redis
 import requests
 from functools import wraps
+from typing import Callable
 
 
 red = redis.Redis()
 
 
-def url_count(method):
-    """decorator for get_page function"""
+def data_cacher(method: Callable) -> Callable:
+    """Returns a Callable that caches the output of a function."""
     @wraps(method)
-    def wrapper(url):
-        """wrapper for get_page function"""
-        key = "cached:" + url
-        cached_value = red.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # key for counting the number of times a URL was accessed
-        key_count = "count:" + url
-        html_content = method(url)
-
-        red.incr(key_count)
-        red.set(key, html_content, ex=10)
-        red.expire(key, 10)
-        return html_content
+    def wrapper(url) -> str:
+        """Returns a string."""
+        red.incr(f'count:{url}')
+        result = red.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        red.set(f'count:{url}', 0)
+        red.setex(f'result:{url}', 10, result)
+        return result
     return wrapper
 
 
-@url_count
+@data_cacher
 def get_page(url: str) -> str:
-    """returns the content of a URL"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """Returns the HTML content of a page."""
+    return requests.get(url).text
